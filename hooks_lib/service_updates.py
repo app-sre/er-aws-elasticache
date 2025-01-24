cbbdc99887ea29a7a2c2d13aa8d53e313ee16d4f
Christian Assing <cassing@redhat.com>
2025-01-24 12:57:30 +0100

    :sparkles: service updates support

---
+++ hooks_lib/service_updates.py
@@ -0,0 +1,81 @@
+import logging
+import time
+from collections.abc import Sequence
+from dataclasses import dataclass
+from datetime import datetime
+
+from hooks_lib.aws_api import AWSApi
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ServiceUpdate:
+    """Service Update Information"""
+
+    name: str
+    release_date: datetime
+    severity: str
+    status: str
+    type: str
+
+
+class ServiceUpdatesManager:
+    """This class manages AWS ElastiCache service updates."""
+
+    def __init__(
+        self,
+        replication_group_id: str,
+        region: str,
+        aws_api_class: type[AWSApi] = AWSApi,
+    ) -> None:
+        self.replication_group_id = replication_group_id
+        self.aws_api = aws_api_class(config_options={"region_name": region})
+
+    @property
+    def update_in_progress(self) -> bool:
+        """Check if an update is in progress."""
+        return bool(
+            self.aws_api.get_service_updates(
+                replication_group_id=self.replication_group_id,
+                status=["waiting-to-start", "in-progress", "scheduling", "stopping"],
+            )
+        )
+
+    def service_updates(
+        self, severities: Sequence[str], released_before: datetime
+    ) -> list[ServiceUpdate]:
+        """Get a list of all available service updates ordered by release date (most recent first)."""
+        return [
+            ServiceUpdate(
+                name=u["ServiceUpdateName"],
+                release_date=u["ServiceUpdateReleaseDate"],
+                severity=u["ServiceUpdateSeverity"],
+                status=u["UpdateActionStatus"],
+                type=u["ServiceUpdateType"],
+            )
+            for u in self.aws_api.get_service_updates(
+                replication_group_id=self.replication_group_id,
+                status=["not-applied", "scheduled", "stopped"],
+            )
+            if u["ServiceUpdateSeverity"] in severities
+            and u["ServiceUpdateReleaseDate"] < released_before
+        ]
+
+    def apply_service_update(
+        self, service_update: ServiceUpdate, *, wait_for_completion: bool = False
+    ) -> None:
+        """Apply a service update."""
+        if self.update_in_progress:
+            raise RuntimeError("An update is already in progress.")
+
+        self.aws_api.batch_apply_service_updates(
+            replication_group_id=self.replication_group_id,
+            service_update_name=service_update.name,
+        )
+
+        if wait_for_completion:
+            logger.info("Waiting for service update to complete...")
+            while self.update_in_progress:
+                print(".", end="", flush=True)  # noqa: T201
+                time.sleep(30)
---
+++ tests/hooks/test_hooks_post_apply.py
@@ -0,0 +1,131 @@
+# ruff: noqa: DTZ001, DTZ005
+from datetime import datetime as dt
+
+import pytest
+from external_resources_io.terraform import (
+    Change,
+    ResourceChange,
+    TerraformJsonPlanParser,
+)
+from pytest_mock import MockerFixture
+
+from er_aws_elasticache.app_interface_input import AppInterfaceInput
+from hooks.post_apply import (
+    default_cooldown,
+    dry_run_mode,
+    main,
+    terraform_changes,
+)
+from hooks_lib.service_updates import ServiceUpdate
+
+SERVICE_UPDATE_ITEM = ServiceUpdate(
+    name="update-1",
+    release_date=dt.now(),
+    severity="critical",
+    status="not-applied",
+    type="security",
+)
+
+
+@pytest.fixture
+def mock_plan(mocker: MockerFixture) -> TerraformJsonPlanParser:
+    plan = mocker.MagicMock(spec=TerraformJsonPlanParser)
+    return plan()
+
+
+@pytest.mark.parametrize(
+    ("plan_changes", "expected_result"),
+    [
+        ([], False),
+        ([ResourceChange(change=Change(actions=["create"], after_unknown=None))], True),
+    ],
+)
+def test_terraform_changes(
+    plan_changes: list[ResourceChange],
+    *,
+    expected_result: bool,
+    mock_plan: TerraformJsonPlanParser,
+) -> None:
+    mock_plan.plan.resource_changes = plan_changes
+    assert terraform_changes(mock_plan) == expected_result
+
+
+@pytest.mark.parametrize(
+    ("environment", "expected_cooldown"),
+    [
+        ("production", 14),
+        ("staging", 7),
+        ("dev", 5),
+        ("stage", 7),
+    ],
+)
+def test_default_cooldown(
+    environment: str, expected_cooldown: int, ai_input: AppInterfaceInput
+) -> None:
+    ai_input.data.environment = environment
+    assert default_cooldown(ai_input.data.environment) == expected_cooldown
+
+
+@pytest.mark.parametrize(
+    ("changes", "expected_exit"),
+    [
+        (True, 1),
+        (False, None),
+    ],
+)
+def test_dry_run_mode(
+    mocker: MockerFixture, *, changes: bool, expected_exit: int | None
+) -> None:
+    service_updates = [SERVICE_UPDATE_ITEM]
+    sys_exit_mock = mocker.patch("sys.exit")
+
+    dry_run_mode(service_updates, changes=changes)
+
+    if expected_exit:
+        sys_exit_mock.assert_called_once_with(expected_exit)
+    else:
+        sys_exit_mock.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    (
+        "service_updates",
+        "terraform_changes_flag",
+        "dry_run_flag",
+        "expected_apply_call",
+    ),
+    [
+        ([], False, True, False),
+        ([SERVICE_UPDATE_ITEM], False, True, False),
+        ([SERVICE_UPDATE_ITEM], True, False, False),
+        ([SERVICE_UPDATE_ITEM], False, False, True),
+        ([SERVICE_UPDATE_ITEM], True, True, False),
+    ],
+)
+def test_main(  # noqa: PLR0913
+    mocker: MockerFixture,
+    service_updates: list[ServiceUpdate],
+    *,
+    terraform_changes_flag: bool,
+    dry_run_flag: bool,
+    expected_apply_call: bool,
+    ai_input: AppInterfaceInput,
+    mock_plan: TerraformJsonPlanParser,
+) -> None:
+    mock_service_updates_manager = mocker.patch(
+        "hooks.post_apply.ServiceUpdatesManager"
+    )
+    mock_service_updates_manager.return_value.service_updates.return_value = (
+        service_updates
+    )
+    mocker.patch(
+        "hooks.post_apply.terraform_changes", return_value=terraform_changes_flag
+    )
+    mocker.patch("hooks.post_apply.dry_run_mode")
+
+    main(mock_plan, ai_input, dry_run=dry_run_flag)
+
+    if expected_apply_call:
+        mock_service_updates_manager.return_value.apply_service_update.assert_called_once()
+    else:
+        mock_service_updates_manager.return_value.apply_service_update.assert_not_called()
